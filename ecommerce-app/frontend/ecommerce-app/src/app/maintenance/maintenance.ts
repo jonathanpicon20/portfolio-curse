440b54a1c3bdd9ad42e4d00588da04fe59db317a
jonathanpicon20 <jonathan.picon20@gmail.com>
2025-06-09 16:14:23 -0500

    Add frontend + page-maintenance

---
+++ ecommerce-app/frontend/ecommerce-app/src/app/maintenance/maintenance.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance',
+  imports: [],
+  templateUrl: './maintenance.html',
+  styleUrl: './maintenance.scss'
+})
+export class Maintenance {
+
+
+}
